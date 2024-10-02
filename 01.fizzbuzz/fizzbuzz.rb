e779571cf42253d2322af7cd83f7af89198a7e18
mochiz <mochi.hiro43@gmail.com>
2024-10-02 20:23:44 +0900

    [fix] rubocopで整形した

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,12 +1,12 @@
 # 1〜20まで数えて3の倍数の時はFizz、5の倍数の時はBuzz、3と5の倍数の時はFizzBuzzと表示する
 20.times.each do |i|
-  i+=1 
-  if i % 3 == 0 && i % 5 == 0
-    puts "FizzBuzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  elsif i % 5 == 0
-    puts "Buzz"
+  i += 1
+  if (i % 3).zero? && (i % 5).zero?
+    puts 'FizzBuzz'
+  elsif (i % 3).zero?
+    puts 'Fizz'
+  elsif (i % 5).zero?
+    puts 'Buzz'
   else
     puts i
   end
