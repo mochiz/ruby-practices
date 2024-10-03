42bfdeedf3134a119cd0e92b0c17cfee35fb52f4
mochiz <mochi.hiro43@gmail.com>
2024-10-03 17:52:54 +0900

    [wip] ひとまず旧ルールのボーリングプログラムを実装した

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'debug'
+
 # % ./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5
 # 139
 # % ./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X
@@ -35,13 +37,33 @@ shots.each_slice(2) do |s|
 end
 
 point = 0
-frames.each do |frame|
-  point += if frame[0] == 10 # strike
-             30
-           elsif frame.sum == 10 # spare
-             frame[0] + 10
-           else
-             frame.sum
-           end
+
+frames.each_with_index do |frame, index|
+  next_frame = frames[index + 1]
+  add_point = if index > 8
+                frame.sum
+              elsif frame[0] == 10
+                pp 'strike'
+                # ストライクの場合は次の2投分を加算する
+                if next_frame
+                  if next_frame[0] == 10 && frames[index + 2]
+                    frame.sum + next_frame.sum + frames[index + 2][0]
+                  else
+                    frame.sum + next_frame.sum
+                  end
+                else
+                  frame.sum
+                end # strike
+              elsif frame.sum == 10 # spare
+                pp 'spare'
+                # スペアの場合は次の1投分を加算する
+                frame.sum + next_frame[0]
+              else
+                frame.sum
+              end
+
+  pp add_point
+  point += add_point
+  pp point
 end
 puts point
