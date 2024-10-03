d28ad0c7ae01a328de348aad348a0c2d29e5366a
mochiz <mochi.hiro43@gmail.com>
2024-10-03 15:26:03 +0900

    [p02] 指定の年月のカレンダーを表示する土台を作成した

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,24 +3,35 @@
 require 'date'
 
 # $ ./cal.rb -m 11
-#       11月 2019        
-# 日 月 火 水 木 金 土  
-#                 1  2  
-#  3  4  5  6  7  8  9  
-# 10 11 12 13 14 15 16  
-# 17 18 19 20 21 22 23  
+#       11月 2019
+# 日 月 火 水 木 金 土
+#                 1  2
+#  3  4  5  6  7  8  9
+# 10 11 12 13 14 15 16
+# 17 18 19 20 21 22 23
 # 24 25 26 27 28 29 30
 
 # -m  -y で月と年を指定してカレンダーを表示できるようにする
-month = nil
-year = nil
+arg_month = nil
+arg_year = nil
 ARGV.each_with_index do |arg, index|
   if arg == '-m'
-    month = ARGV[index + 1]
+    arg_month = ARGV[index + 1]
   elsif arg == '-y'
-    year = ARGV[index + 1]
+    arg_year = ARGV[index + 1]
   end
 end
 
-puts "Month: #{month}"
-puts "Year: #{year}"
+year = arg_year&.to_i || Date.today.year
+month = arg_month&.to_i || Date.today.month
+
+start_date = Date.new(year, month, 1)
+end_date = Date.new(year, month, -1)
+
+puts "#{month}月 #{year}"
+puts '日 月 火 水 木 金 土'
+# TODO: 表示を調整する
+(start_date..end_date).each do |day|
+  print day.day.to_s.rjust(3)
+  print "\n" if day.saturday?
+end
