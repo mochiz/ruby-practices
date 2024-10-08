3a3615efb4b4cbb0630ea75ad94438d0ea0601ee
mochiz <mochi.hiro43@gmail.com>
2024-10-08 23:42:36 +0900

    [fix] カレンダー描画プログラムを修正した
    - 不要な左端の空白を除外した
    - コマンドラインオプションをoptparseで受け取るよう修正した
    - 消し忘れのdates配列を削除した

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/env ruby
 
 require 'date'
+require 'optparse'
 
 # $ ./cal.rb -m 11
 #       11月 2019
@@ -12,28 +13,19 @@ require 'date'
 # 24 25 26 27 28 29 30
 
 # -m  -y で月と年を指定してカレンダーを表示できるようにする
-arg_month = nil
-arg_year = nil
-ARGV.each_with_index do |arg, index|
-  if arg == '-m'
-    arg_month = ARGV[index + 1]
-  elsif arg == '-y'
-    arg_year = ARGV[index + 1]
-  end
-end
-
-year = arg_year&.to_i || Date.today.year
-month = arg_month&.to_i || Date.today.month
-
+options = ARGV.getopts('m:', 'y:')
+year = options['y']&.to_i || Date.today.year
+month = options['m']&.to_i || Date.today.month
+# 指定年月の初日と最終日を取得
 start_date = Date.new(year, month, 1)
 end_date = Date.new(year, month, -1)
-dates = (start_date..end_date).to_a
 
+# 指定年月のカレンダーを描画する
 puts "#{month}月 #{year}".center(21)
-puts ' 日 月 火 水 木 金 土 '
+puts '日 月 火 水 木 金 土'
 print ' '.rjust(3) * start_date.wday # 月の初日の曜日まで空白で埋め
 (start_date..end_date).each do |day|
-  print day.day.to_s.rjust(3)
-  print "\n" if day.saturday?
+  print "#{day.day.to_s.rjust(2)} "
+  puts if day.saturday?
 end
 print "\n"
