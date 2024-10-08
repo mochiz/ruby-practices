30f645d85a568be73c155f923f70f21a19393e97
mochiz <mochi.hiro43@gmail.com>
2024-10-08 13:54:44 +0900

    [fix] 不要なindex初期値設定を除外した

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,8 +1,7 @@
 #! /usr/bin/env ruby
 
 # 1〜20まで数えて3の倍数の時はFizz、5の倍数の時はBuzz、3と5の倍数の時はFizzBuzzと表示する
-20.times.each do |i|
-  i += 1
+(1..20).each do |i|
   if (i % 3).zero? && (i % 5).zero?
     puts 'FizzBuzz'
   elsif (i % 3).zero?
