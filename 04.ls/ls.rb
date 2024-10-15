49f83ee87ebb03bdadd091e59a98ddf4e3c9f627
mochiz <mochi.hiro43@gmail.com>
2024-10-15 11:45:52 +0900

    [fix] itemsの名前を変更した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,16 +4,16 @@
 COLUMN_SIZE = 3
 PADDING_MARGIN = 1
 
-file_names = Dir.children(__dir__)
-items = file_names.sort.reject { |item| item.start_with?('.') }
+items = Dir.children(__dir__)
+sorted_items = items.sort.reject { |item| item.start_with?('.') }
 
-padding_size = items.map(&:length).max + PADDING_MARGIN
+padding_size = sorted_items.map(&:length).max + PADDING_MARGIN
 
-lack_count = items.length % COLUMN_SIZE
-items.concat([''] * (COLUMN_SIZE - lack_count)) if lack_count.positive?
+lack_count = sorted_items.length % COLUMN_SIZE
+sorted_items.concat([''] * (COLUMN_SIZE - lack_count)) if lack_count.positive?
 
-row_size = (items.length / COLUMN_SIZE)
-matrix = items.each_slice(row_size).to_a.transpose
+row_size = (sorted_items.length / COLUMN_SIZE)
+matrix = sorted_items.each_slice(row_size).to_a.transpose
 matrix.each do |row_items|
   row_items.each do |item|
     print item.ljust(padding_size)
