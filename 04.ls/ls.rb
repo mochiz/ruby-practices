a508b4d12c97595d30538b2605b0464c4c1c77f5
mochiz <mochi.hiro43@gmail.com>
2024-10-17 00:32:50 +0900

    item という変数名を file_name に差し替えた

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,19 +4,19 @@
 COLUMN_SIZE = 3
 PADDING_MARGIN = 1
 
-items = Dir.children(__dir__)
-sorted_items = items.sort.reject { |item| item.start_with?('.') }
+file_names = Dir.children(__dir__)
+sorted_file_names = file_names.sort.reject { |file_name| file_name.start_with?('.') }
 
-padding_size = sorted_items.map(&:length).max + PADDING_MARGIN
+padding_size = sorted_file_names.map(&:length).max + PADDING_MARGIN
 
-lack_count = sorted_items.length % COLUMN_SIZE
-sorted_items.concat([''] * (COLUMN_SIZE - lack_count)) if lack_count.positive?
+lack_count = sorted_file_names.length % COLUMN_SIZE
+sorted_file_names.concat([''] * (COLUMN_SIZE - lack_count)) if lack_count.positive?
 
-row_size = (sorted_items.length / COLUMN_SIZE)
-matrix = sorted_items.each_slice(row_size).to_a.transpose
-matrix.each do |row_items|
-  row_items.each do |item|
-    print item.ljust(padding_size)
+row_size = (sorted_file_names.length / COLUMN_SIZE)
+matrix = sorted_file_names.each_slice(row_size).to_a.transpose
+matrix.each do |row_file_names|
+  row_file_names.each do |file_name|
+    print file_name.ljust(padding_size)
   end
   puts
 end
