1346aa416266ea67a19c89d62db2e82d40b26abd
mochiz <mochi.hiro43@gmail.com>
2024-10-17 11:54:32 +0900

    sorted_file_names は冗長なので file_names に変更した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -8,7 +8,7 @@ PADDING_MARGIN = 1
 
 options = ARGV.getopts('a')
 
-def get_sorted_file_names(options)
+def get_file_names(options)
   file_names = Dir.entries(__dir__)
   if options['a']
     file_names.sort
@@ -17,14 +17,14 @@ def get_sorted_file_names(options)
   end
 end
 
-sorted_file_names = get_sorted_file_names(options)
-padding_size = sorted_file_names.map(&:length).max + PADDING_MARGIN
+file_names = get_file_names(options)
+padding_size = file_names.map(&:length).max + PADDING_MARGIN
 
-lack_count = sorted_file_names.length % COLUMN_SIZE
-sorted_file_names.concat([''] * (COLUMN_SIZE - lack_count)) if lack_count.positive?
+lack_count = file_names.length % COLUMN_SIZE
+file_names.concat([''] * (COLUMN_SIZE - lack_count)) if lack_count.positive?
 
-row_size = (sorted_file_names.length / COLUMN_SIZE)
-matrix = sorted_file_names.each_slice(row_size).to_a.transpose
+row_size = (file_names.length / COLUMN_SIZE)
+matrix = file_names.each_slice(row_size).to_a.transpose
 matrix.each do |row_file_names|
   row_file_names.each do |file_name|
     print file_name.ljust(padding_size)
