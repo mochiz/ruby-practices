749afbd5a6d8a0d1b89ebfe1ed2dbeaf44b0fe87
mochiz <mochi.hiro43@gmail.com>
2024-10-18 12:33:02 +0900

    ls.rbについて指摘箇所を修正した
    - optionsが紛らわしいので ARGVで取得したオプションはargv_optionsと別名にした
    - get_ prefixはrubyらしくないので削除した
    - sort 処理がDRYじゃないのでsorted_file_names 変数に保持するよう修正した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,28 +6,34 @@ require 'optparse'
 COLUMN_SIZE = 3
 PADDING_MARGIN = 1
 
-options = ARGV.getopts('a')
+argv_options = ARGV.getopts('a')
 
-def get_file_names(options)
+def main(options = {})
+  file_names = file_names(options)
+
+  padding_size = file_names.map(&:length).max + PADDING_MARGIN
+
+  lack_count = file_names.length % COLUMN_SIZE
+  file_names.concat([''] * (COLUMN_SIZE - lack_count)) if lack_count.positive?
+
+  row_size = (file_names.length / COLUMN_SIZE)
+  matrix = file_names.each_slice(row_size).to_a.transpose
+  matrix.each do |row_file_names|
+    row_file_names.each do |file_name|
+      print file_name.ljust(padding_size)
+    end
+    puts
+  end
+end
+
+def file_names(options = {})
   file_names = Dir.entries(__dir__)
+  sorted_file_names = file_names.sort
   if options['a']
-    file_names.sort
+    sorted_file_names
   else
-    file_names.sort.reject { |file_name| file_name.start_with?('.') }
+    sorted_file_names.reject { |file_name| file_name.start_with?('.') }
   end
 end
 
-file_names = get_file_names(options)
-padding_size = file_names.map(&:length).max + PADDING_MARGIN
-
-lack_count = file_names.length % COLUMN_SIZE
-file_names.concat([''] * (COLUMN_SIZE - lack_count)) if lack_count.positive?
-
-row_size = (file_names.length / COLUMN_SIZE)
-matrix = file_names.each_slice(row_size).to_a.transpose
-matrix.each do |row_file_names|
-  row_file_names.each do |file_name|
-    print file_name.ljust(padding_size)
-  end
-  puts
-end
+main(argv_options)
